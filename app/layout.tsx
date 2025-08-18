db9af49d95500ea221cabb5c2f4dd3a16845f662
Meeky Hwang <mhwang@ndevr.io>
2025-08-18 17:47:49 -0400

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,15 +40,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics 4 */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R6TZEXJXBM"
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GT-NNXPFSK');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R6TZEXJXBM', {
+                page_title: document.title,
+                page_location: window.location.href,
+              });
             `,
           }}
         />
@@ -57,16 +63,6 @@ export default function RootLayout({
         <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/2143432.js"></script>
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GT-NNXPFSK"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
